b58c09e09f287e1be08e99f5c8397c763e535f33
Jason Gorman <email@jasongorman.uk>
2022-01-20 14:21:51 +0000

    Update update-dns-record.sh

--- update-dns-record.sh
+++ update-dns-record.sh
@@ -1,14 +1,20 @@
-MY_PUBLIC_IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 ZONE_ID=""
 RECORD_ID=""
 RECORD_NAME=""
 AUTH_TOKEN=""
+MY_PUBLIC_IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+CURRENT_IP="$(dig +short ${RECORD_NAME} @1.1.1.1)"
 TTL=60
 
-echo "Updating Cloudflare with public IP ${myip}"
+if [ "$MY_PUBLIC_IP" != "$CURRENT_IP" ]; then
 
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/'${ZONE_ID}'/dns_records/'${RECORD_ID}'" \
-        -H "Authorization: Bearer '${AUTH_TOKEN}'" \
-        -H "Content-Type: application/json" \
-        --data '{"type":"A","name":"'${RECORD_NAME}'","content":"'${MY_PUBLIC_IP}'","ttl":'${TTL}',"proxied":false}'
+        echo "Updating Cloudflare ${RECORD_NAME} with public IP ${MY_PUBLIC_IP}"
+
+        curl -X PUT "https://api.cloudflare.com/client/v4/zones/'${ZONE_ID}'/dns_records/'${RECORD_ID}'" \
+                -H "Authorization: Bearer '${AUTH_TOKEN}'" \
+                -H "Content-Type: application/json" \
+                --data '{"type":"A","name":"'${RECORD_NAME}'","content":"'${MY_PUBLIC_IP}'","ttl":'${TTL}',"proxied":false}'
+else
+    echo "IP is the same, no update required."
+fi
 echo
