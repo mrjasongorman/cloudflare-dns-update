bbd2e37e22c85140d8e7551c51d90d6166b4434d
Jason Gorman <email@jasongorman.uk>
2022-01-20 10:32:38 +0000

    Update update-dns-record.sh

--- update-dns-record.sh
+++ update-dns-record.sh
@@ -8,7 +8,7 @@ TTL=60
 echo "Updating Cloudflare with public IP ${myip}"
 
 curl -X PUT "https://api.cloudflare.com/client/v4/zones/'${ZONE_ID}'/dns_records/'${RECORD_ID}'" \
-        -H "Authorization: Bearer AUTH_TOKEN" \
+        -H "Authorization: Bearer '${AUTH_TOKEN}'" \
         -H "Content-Type: application/json" \
         --data '{"type":"A","name":"'${RECORD_NAME}'","content":"'${MY_PUBLIC_IP}'","ttl":'${TTL}',"proxied":false}'
 echo
