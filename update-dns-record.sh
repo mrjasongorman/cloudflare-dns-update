3c8d7154f4adca05f3e4b185e47d35b680b57f43
Jason Gorman <email@jasongorman.uk>
2022-02-27 17:36:38 +0000

    Update to more reliable public IP resolver

--- update-dns-record.sh
+++ update-dns-record.sh
@@ -3,18 +3,21 @@ RECORD_ID=""
 RECORD_NAME=""
 AUTH_TOKEN=""
 MY_PUBLIC_IP="$(dig whoami.cloudflare ch txt @1.1.1.1 +short)"
-CURRENT_IP="$(dig +short ${RECORD_NAME} @1.1.1.1)"
+CURRENT_IP="\"$(dig +short ${RECORD_NAME} @1.1.1.1)\""
 TTL=60
+
+echo "Public IP: ${MY_PUBLIC_IP}"
+echo "IP currently in DNS: ${CURRENT_IP}"
 
 if [ "$MY_PUBLIC_IP" != "$CURRENT_IP" ]; then
 
         echo "Updating Cloudflare ${RECORD_NAME} with public IP ${MY_PUBLIC_IP}"
 
-        curl -X PUT "https://api.cloudflare.com/client/v4/zones/'${ZONE_ID}'/dns_records/'${RECORD_ID}'" \
-                -H "Authorization: Bearer '${AUTH_TOKEN}'" \
+        curl -X PUT "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${RECORD_ID}" \
+                -H "Authorization: Bearer ${AUTH_TOKEN}" \
                 -H "Content-Type: application/json" \
-                --data '{"type":"A","name":"'${RECORD_NAME}'","content":"'${MY_PUBLIC_IP}'","ttl":'${TTL}',"proxied":false}'
+                --data "{\"type\":\"A\",\"name\":\"${RECORD_NAME}\",\"content\":${MY_PUBLIC_IP},\"ttl\":${TTL},\"proxied\":false}"
 else
-    echo "IP is the same, no update required."
+    echo "IP addresses are the same, no update required."
 fi
 echo
